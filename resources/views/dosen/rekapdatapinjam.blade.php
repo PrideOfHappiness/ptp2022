1c7674bcc020f7b3a298fb368a06ba15606ab81d
Renaldi <soeryadi.renaldi@gmail.com>
2022-04-20 19:33:48 +0700

    revision

--- resources/views/dosen/rekapdatapinjam.blade.php
+++ resources/views/dosen/rekapdatapinjam.blade.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>UKDW P.P | Dashboard</title>
+  <title>UKDW P.P | Riwayat Peminjaman</title>
 
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
@@ -69,15 +69,15 @@
       <!-- Sidebar user panel (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
-          <img src="style/dist/img/avatar02.jpg" class="img-circle elevation-2" alt="User Image">
+          <img src="style/dist/img/avatar01.jpg" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
-          <a href="#" class="d-block">Renaldi Soeryadi</a>
+          <a href="#" class="d-block">Jevon Hendro S</a>
         </div>
       </div>
 
 <!-- Sidebar Menu -->
-<nav class="mt-2">
+      <nav class="mt-2">
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
 <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
@@ -88,136 +88,125 @@
                 Dashboard
               </p>
             </a>
-</li>
           <li class="nav-item">
-            <a href="pages/widgets.html" class="nav-link active">
+            <a href="pages/widgets.html" class="nav-link">
               <i class="nav-icon fas fa-th"></i>
               <p>
                 Data Barang
               </p>
             </a>
-</li>
+          </li>
           <li class="nav-item">
             <a href="#" class="nav-link">
               <i class="nav-icon fas fa-copy"></i>
               <p>
-                Buat Notifikasi
+                Data Peminjaman
               </p>
             </a>
-</li>
-            <li class="nav-item">
+            <li class="nav-item menu-open">
             <a href="#" class="nav-link">
-            <i class="nav-icon fas fa-user"></i>
-            <p>
-                Data Peminjam
-            </p>
+              <i class="nav-icon fas fa-edit"></i>
+              <p>
+                Ijin Pengajuan
+                <i class="right fas fa-angle-left"></i>
+                </p>
             </a>
-</li>
+            <ul class="nav nav-treeview">
             <li class="nav-item">
-            <a href="#" class="nav-link">
-            <i class="nav-icon fas fa-table"></i>
-            <p>
-                Laporan Pengembalian.
-            </p>
+                <a href="fpinjam.blade.php" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Peminjaman</p>
+                </a>
+              </li>
+            <li class="nav-item">
+                <a href="/welcome/fpinjam.blade.php" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Booking</p>
+                </a>
+              </li>
+              </ul>
+            </li> 
+                <li class="nav-item">
+                    <a href="#" class="nav-link active">
+              <i class="nav-icon fas fa-table"></i>
+              <p>
+                Riwayat Peminjaman
+              </p>
             </a>
-</li>
+   
+      <!-- /.sidebar-menu -->
     </div>
-    </aside>
-
-  <!-- Content Wrapper. Contains page content -->
-  <div class="content-wrapper">
+</aside>
+    <!-- /.sidebar -->
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
     <!-- Content Header (Page header) -->
-    <div class="content-header">
+    <section class="content-header">
       <div class="container-fluid">
         <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1 class="m-0">Dashboard</h1>
-          </div><!-- /.col -->
-          <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item active">Dashboard</li>
-            </ol>
-          </div><!-- /.col -->
-        </div><!-- /.row -->
-      </div><!-- /.container-fluid -->
-    </div>
-    <!-- /.content-header -->
-
-    <!-- Main content -->
-    <section class="content">
-      <div class="container-fluid">
-        <!-- Small boxes (Stat box) -->
-        <div class="row">
-          <div class="col-lg-3 col-6">
-            <!-- small box -->
-            <div class="small-box bg-info">
-              <div class="inner">
-                <h3>50</h3>
-
-                <p>Barang Tersedia</p>
-              </div>
-              <div class="icon">
-                <i class="nav-icon fas fa-th"></i>
-              </div>
-              <a href="#" class="small-box-footer">Lihat Detail <i class="fas fa-arrow-circle-right"></i></a>
-            </div>
+          <div class="col-sm-12">
+            <h1 style="text-align:center;">Riwayat Peminjaman</h1>
           </div>
-          <!-- ./col -->
-          <div class="col-lg-3 col-6">
-            <!-- small box -->
-            <div class="small-box bg-success">
-              <div class="inner">
-                <h3>5<sup style="font-size: 20px"></sup></h3>
-
-                <p>Barang yang dipinjam</p>
-              </div>
-              <div class="icon">
-                <i class="nav-icon fas fa-book"></i>
-              </div>
-              <a href="#" class="small-box-footer">Lihat Detail <i class="fas fa-arrow-circle-right"></i></a>
-            </div>
-          </div>
-          <!-- ./col -->
-          <div class="col-lg-3 col-6">
-            <!-- small box -->
-            <div class="small-box bg-warning">
-            
-              <div class="inner">
-                <h3>3</h3>
-
-                <p>Notifikasi Pengajuan</p>
-              </div>
-              <div class="icon">
-                <i class="far fa-bell"></i>
-              </div>
-              <a href="#" class="small-box-footer">Lihat Detail <i class="fas fa-arrow-circle-right"></i></a>
-            </div>
-          </div>
-          <!-- ./col -->
-          <div class="col-lg-3 col-6">
-            <!-- small box -->
-                </div>
-                <!-- /. tools -->
-              </div>
-              <!-- /.card-header -->
-              <div class="card-body pt-0">
-                <!--The calendar -->
-                <div id="calendar" style="width: 100%"></div>
-              </div>
-              <!-- /.card-body -->
-            </div>
-            <!-- /.card -->
-          </section>
-          <!-- right col -->
         </div>
-        <!-- /.row (main row) -->
       </div><!-- /.container-fluid -->
     </section>
-    <!-- /.content -->
-  </div>
-  <!-- /.content-wrapper -->
-  <footer class="main-footer">
+<!-- Main content -->
+    <section class="content">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-12">
+            <div class="card">
+              <div class="card-header">
+                <h3 class="card-title">Data Peminjaman yang anda lakukan</h3>
+              </div>
+              <!-- /.card-header -->
+              <div class="card-body">
+                <table id="example2" class="table table-bordered table-hover">
+                  <thead>
+                  <tr>
+                    <th>Tanggal Pinjam</th>
+                    <th>Tanggal Selesai</th>
+                    <th>Peralatan</th>
+                    <th>Status</th>
+                  </tr>
+                  </thead>
+                  <tbody>
+                  <tr>
+                    <td>28/03/22</td>
+                    <td>30/03/22</td>
+                    <td>WebCam</td>
+                    <td>Sudah Kembali</td>
+            </tr>
+                  <tr>
+                    <td>28/03/22</td>
+                    <td>30/03/22</td>
+                    <td>Headset</td>
+                    <td>Sudah Kembali</td>
+            </tr>
+                  <tr>
+                    <td>28/03/22</td>
+                    <td>30/03/22</td>
+                    <td>Modem</td>
+                    <td>Sudah Kembali</td>
+            </tr>
+                  <tr>
+                    <td>28/03/22</td>
+                    <td>30/03/22</td>
+                    <td>Mic</td>
+                    <td>Sudah Kembali</td>
+            </tr>
+        </tfoot>
+    </table>
+    </div>    
+</div>
+    </div>    
+</div>    
+    </div>
+</div> 
+
+</aside> 
+<!-- /.Bagian Copyright content-wrapper -->
+<footer class="main-footer">
     <strong>Copyright &copy; Renaldi-Jevon <a href="https://adminlte.io">UKDW P.P</a>.</strong>
     All rights reserved.
     <div class="float-right d-none d-sm-inline-block">
@@ -228,10 +217,11 @@
   <!-- Control Sidebar -->
   <aside class="control-sidebar control-sidebar-dark">
     <!-- Control sidebar content goes here -->
-  </aside>
+</aside>
   <!-- /.control-sidebar -->
 </div>
 <!-- ./wrapper -->
+<!-- Content Wrapper. Contains page content -->
 
 <!-- jQuery -->
 <script src="{{ asset('style/plugins/jquery/jquery.min.js') }}"></script>
